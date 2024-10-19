d3b882c7f53e5cb44c5650f3ff75260a4b742853
smolgeorgie <ilona.van.oosbree@hva.nl>
2024-10-19 14:01:55 +0200

    Loaded in Hygraph data
    Close #1

---
+++ myproject/app/pages/index.mjs
@@ -0,0 +1,31 @@
+// /app/pages/program.mjs
+
+export default function Program({ html, state }) {
+    const { store } = state
+    const { program } = store
+  
+    return html`
+      <h1>${program.title}</h1>
+      <h2>${program.subtitle}</h2>
+  
+      <div>
+        ${program.content.html}
+      </div>
+  
+      <h3>Semesters</h3>
+      <ul>
+        ${program.semesters.map(semester => `
+          <li>
+            <strong>${semester.title}</strong>
+            <ul>
+              ${semester.sprints.map(sprint => `
+                <li>
+                  Sprint ${sprint.sprintNumber}: ${sprint.title} (Start date: ${sprint.startdate})
+                </li>
+              `).join('')}
+            </ul>
+          </li>
+        `).join('')}
+      </ul>
+    `
+  }
